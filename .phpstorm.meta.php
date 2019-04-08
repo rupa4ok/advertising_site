9f98567dd6f64c643f8baafc4338afa59146da73
rupak <rupak4@yandex.ru>
2019-04-08 23:29:05 +0300

    6. Хлебные крошки

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-04-03 10:13:24.
+    * Generated on 2019-04-03 21:56:49.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- app/Http/Controllers/Cabinet/CabinetController.php
+++ app/Http/Controllers/Cabinet/CabinetController.php
@@ -11,11 +11,7 @@ class CabinetController extends Controller
     {
         $this->middleware('auth');
     }
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
     public function index()
     {
         return view('cabinet.home');
