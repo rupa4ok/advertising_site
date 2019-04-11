e1a10f0f52b324ff75eb42ed3f8bb6d7ad280be3
rupa4ok <rupak8@yandex.ru>
2019-04-11 22:56:57 +0300

    админ-панель

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Cabinet;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('admin.home');
+    }
+}
