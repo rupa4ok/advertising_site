3a948e09aedefe7de1762cd8139b6d3438363185
rupa4ok <rupak4@yandex.ru>
2019-05-02 14:43:29 +0300

    Add user permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,29 +2,22 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
-
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
+    
     public function boot()
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin-panel', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
