2082d946e59cacfaa1a2ab56237a90ec07773938
rupa4ok <rupak4@yandex.ru>
2019-05-03 00:17:05 +0300

    Add advert CategoryController

---
+++ app/Http/Controllers/Admin/Advert/CategoryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Advert;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+    
+    public function create()
+    {
+        //
+    }
+    
+    public function store(Request $request)
+    {
+        //
+    }
+    
+    public function show($id)
+    {
+        //
+    }
+    
+    public function edit($id)
+    {
+        //
+    }
+    
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    
+    public function destroy($id)
+    {
+        //
+    }
+}
