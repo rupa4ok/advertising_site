a53f1616259645e133e08a74612581e0e64a84d5
rupa4ok <rupak4@yandex.ru>
2019-05-16 18:16:46 +0300

    Add models cache

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        factory(User::class, 50)->create();
+        factory(User::class, 500000)->create();
         $this->call(RegionsTableSeeder::class);
         $this->call(AdvertCategoriesTableSeeder::class);
     }
