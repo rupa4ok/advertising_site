182b5f74e1bab7ab2d895aa83d52c002da98e159
rupa4ok <rupak4@yandex.ru>
2019-06-16 23:39:16 +0300

    Add admin-adverts view

--- routes/web.php
+++ routes/web.php
@@ -93,41 +93,71 @@ Route::group(
 );
 
 Route::group(
-    [
-        'prefix' => 'admin',
-        'as' => 'admin.',
-        'namespace' => 'Admin',
-        'middleware' => ['auth', 'can:admin-panel'],
-    ],
-    function () {
-        Route::get('/', 'HomeController@index')->name('home');
-        Route::resource('users', 'UsersController');
-        Route::resource('regions', 'RegionController');
-    
-        Route::group(['prefix' => 'adverts', 'as' => 'adverts.', 'namespace' => 'Adverts'], function () {
-            Route::resource('categories', 'CategoryController');
-            Route::group(['prefix' => 'categories/{category}', 'as' => 'categories.'], function () {
-                Route::post('/first', 'CategoryController@first')->name('first');
-                Route::post('/up', 'CategoryController@up')->name('up');
-                Route::post('/down', 'CategoryController@down')->name('down');
-                Route::post('/last', 'CategoryController@last')->name('last');
-                Route::resource('attributes', 'AttributeController')->except('index');
-            });
-            Route::group(['prefix' => 'adverts', 'as' => 'adverts.'], function () {
-                Route::get('/', 'AdvertController@index')->name('index');
-                Route::get('/{advert}/edit', 'AdvertController@editForm')->name('edit');
-                Route::put('/{advert}/edit', 'AdvertController@edit');
-                Route::get('/{advert}/photos', 'AdvertController@photosForm')->name('photos');
-                Route::post('/{advert}/photos', 'AdvertController@photos');
-                Route::get('/{advert}/attributes', 'AdvertController@attributesForm')->name('attributes');
-                Route::post('/{advert}/attributes', 'AdvertController@attributes');
-                Route::post('/{advert}/moderate', 'AdvertController@moderate')->name('moderate');
-                Route::get('/{advert}/reject', 'AdvertController@rejectForm')->name('reject');
-                Route::post('/{advert}/reject', 'AdvertController@reject');
-                Route::delete('/{advert}/destroy', 'AdvertController@destroy')->name('destroy');
-            });
-        });
-    }
+	[
+		'prefix' => 'admin',
+		'as' => 'admin.',
+		'namespace' => 'Admin',
+		'middleware' => ['auth', 'can:admin-panel'],
+	],
+	function () {
+		Route::post('/ajax/upload/image', 'UploadController@image')->name('ajax.upload.image');
+		Route::get('/', 'HomeController@index')->name('home');
+		Route::resource('users', 'UsersController');
+		Route::post('/users/{user}/verify', 'UsersController@verify')->name('users.verify');
+		Route::resource('regions', 'RegionController');
+		Route::resource('pages', 'PageController');
+		Route::group(['prefix' => 'pages/{page}', 'as' => 'pages.'], function () {
+			Route::post('/first', 'PageController@first')->name('first');
+			Route::post('/up', 'PageController@up')->name('up');
+			Route::post('/down', 'PageController@down')->name('down');
+			Route::post('/last', 'PageController@last')->name('last');
+		});
+		Route::group(['prefix' => 'adverts', 'as' => 'adverts.', 'namespace' => 'Adverts'], function () {
+			Route::resource('categories', 'CategoryController');
+			Route::group(['prefix' => 'categories/{category}', 'as' => 'categories.'], function () {
+				Route::post('/first', 'CategoryController@first')->name('first');
+				Route::post('/up', 'CategoryController@up')->name('up');
+				Route::post('/down', 'CategoryController@down')->name('down');
+				Route::post('/last', 'CategoryController@last')->name('last');
+				Route::resource('attributes', 'AttributeController')->except('index');
+			});
+			Route::group(['prefix' => 'adverts', 'as' => 'adverts.'], function () {
+				Route::get('/', 'AdvertController@index')->name('index');
+				Route::get('/{advert}/edit', 'AdvertController@editForm')->name('edit');
+				Route::put('/{advert}/edit', 'AdvertController@edit');
+				Route::get('/{advert}/photos', 'AdvertController@photosForm')->name('photos');
+				Route::post('/{advert}/photos', 'AdvertController@photos');
+				Route::get('/{advert}/attributes', 'AdvertController@attributesForm')->name('attributes');
+				Route::post('/{advert}/attributes', 'AdvertController@attributes');
+				Route::post('/{advert}/moderate', 'AdvertController@moderate')->name('moderate');
+				Route::get('/{advert}/reject', 'AdvertController@rejectForm')->name('reject');
+				Route::post('/{advert}/reject', 'AdvertController@reject');
+				Route::delete('/{advert}/destroy', 'AdvertController@destroy')->name('destroy');
+			});
+		});
+		Route::group(['prefix' => 'banners', 'as' => 'banners.'], function () {
+			Route::get('/', 'BannerController@index')->name('index');
+			Route::get('/{banner}/show', 'BannerController@show')->name('show');
+			Route::get('/{banner}/edit', 'BannerController@editForm')->name('edit');
+			Route::put('/{banner}/edit', 'BannerController@edit');
+			Route::post('/{banner}/moderate', 'BannerController@moderate')->name('moderate');
+			Route::get('/{banner}/reject', 'BannerController@rejectForm')->name('reject');
+			Route::post('/{banner}/reject', 'BannerController@reject');
+			Route::post('/{banner}/pay', 'BannerController@pay')->name('pay');
+			Route::delete('/{banner}/destroy', 'BannerController@destroy')->name('destroy');
+		});
+		Route::group(['prefix' => 'tickets', 'as' => 'tickets.'], function () {
+			Route::get('/', 'TicketController@index')->name('index');
+			Route::get('/{ticket}/show', 'TicketController@show')->name('show');
+			Route::get('/{ticket}/edit', 'TicketController@editForm')->name('edit');
+			Route::put('/{ticket}/edit', 'TicketController@edit');
+			Route::post('{ticket}/message', 'TicketController@message')->name('message');
+			Route::post('/{ticket}/close', 'TicketController@close')->name('close');
+			Route::post('/{ticket}/approve', 'TicketController@approve')->name('approve');
+			Route::post('/{ticket}/reopen', 'TicketController@reopen')->name('reopen');
+			Route::delete('/{ticket}/destroy', 'TicketController@destroy')->name('destroy');
+		});
+	}
 );
 
 Route::get('/ajax/regions', 'Ajax\RegionController@get')->name('ajax.regions');
