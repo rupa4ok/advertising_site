95277f8c740d32f51e355cfa8a0cb957ba00d451
rupa4ok <rupak4@yandex.ru>
2019-06-17 06:02:33 +0300

    Add some methods to Advert

--- app/Models/Adverts/Advert/Advert.php
+++ app/Models/Adverts/Advert/Advert.php
@@ -60,50 +60,100 @@ class Advert extends Model
 	public const STATUS_MODERATION = 'moderation';
 	public const STATUS_ACTIVE = 'active';
 	public const STATUS_CLOSED = 'closed';
-	
 	protected $table = 'advert_adverts';
-	
 	protected $guarded = ['id'];
-	
 	protected $casts = [
-		'published_at' => 'datatime',
-		'expired_at'
+		'published_at' => 'datetime',
+		'expires_at' => 'datetime',
 	];
-	
-	public function sendToModeration()
+	public static function statusesList(): array
+	{
+		return [
+			self::STATUS_DRAFT => 'Draft',
+			self::STATUS_MODERATION => 'On Moderation',
+			self::STATUS_ACTIVE => 'Active',
+			self::STATUS_CLOSED => 'Closed',
+		];
+	}
+	public function sendToModeration(): void
 	{
 		if (!$this->isDraft()) {
-			throw new \DomainException('Advert is not draft');
+			throw new \DomainException('Advert is not draft.');
 		}
-		if (!$this->photos()->count()) {
-			throw new \DomainException('Upload photos');
+		if (!\count($this->photos)) {
+			throw new \DomainException('Upload photos.');
 		}
 		$this->update([
 			'status' => self::STATUS_MODERATION,
 		]);
 	}
-	
 	public function moderate(Carbon $date): void
 	{
 		if ($this->status !== self::STATUS_MODERATION) {
-			throw new \DomainException('Advert is not send to moderation');
+			throw new \DomainException('Advert is not sent to moderation.');
 		}
-		
 		$this->update([
 			'published_at' => $date,
-			'expires_at' => $date->copy()->addDay(15),
-			'status' => self::STATUS_MODERATION,
+			'expires_at' => $date->copy()->addDays(15),
+			'status' => self::STATUS_ACTIVE,
 		]);
 	}
-	
 	public function reject($reason): void
 	{
 		$this->update([
 			'status' => self::STATUS_DRAFT,
-			'reject_reason' => $reason
+			'reject_reason' => $reason,
 		]);
 	}
-	
+	public function expire(): void
+	{
+		$this->update([
+			'status' => self::STATUS_CLOSED,
+		]);
+	}
+	public function close(): void
+	{
+		$this->update([
+			'status' => self::STATUS_CLOSED,
+		]);
+	}
+	public function writeClientMessage(int $fromId, string $message): void
+	{
+		$this->getOrCreateDialogWith($fromId)->writeMessageByClient($fromId, $message);
+	}
+	public function writeOwnerMessage(int $toId, string $message): void
+	{
+		$this->getDialogWith($toId)->writeMessageByOwner($this->user_id, $message);
+	}
+	public function readClientMessages(int $userId): void
+	{
+		$this->getDialogWith($userId)->readByClient();
+	}
+	public function readOwnerMessages(int $userId): void
+	{
+		$this->getDialogWith($userId)->readByOwner();
+	}
+	private function getDialogWith(int $userId): Dialog
+	{
+		$dialog = $this->dialogs()->where([
+			'user_id' => $this->user_id,
+			'client_id' => $userId,
+		])->first();
+		if (!$dialog) {
+			throw new \DomainException('Dialog is not found.');
+		}
+		return $dialog;
+	}
+	private function getOrCreateDialogWith(int $userId): Dialog
+	{
+		if ($userId === $this->user_id) {
+			throw new \DomainException('Cannot send message to myself.');
+		}
+		return $this->dialogs()->firstOrCreate([
+			'user_id' => $this->user_id,
+			'client_id' => $userId,
+		]);
+	}
 	public function getValue($id)
 	{
 		foreach ($this->values as $value) {
@@ -113,75 +163,65 @@ class Advert extends Model
 		}
 		return null;
 	}
-	
 	public function isDraft(): bool
 	{
-		return $this->status = self::STATUS_DRAFT;
+		return $this->status === self::STATUS_DRAFT;
 	}
-	
 	public function isOnModeration(): bool
 	{
 		return $this->status === self::STATUS_MODERATION;
 	}
-	
 	public function isActive(): bool
 	{
-		return $this->status = self::STATUS_ACTIVE;
+		return $this->status === self::STATUS_ACTIVE;
 	}
-	
 	public function isClosed(): bool
 	{
-		return $this->status = self::STATUS_CLOSED;
+		return $this->status === self::STATUS_CLOSED;
 	}
-	
-	public function isModerate(): bool
-	{
-		return $this->status = self::STATUS_MODERATION;
-	}
-	
 	public function user()
 	{
 		return $this->belongsTo(User::class, 'user_id', 'id');
 	}
-	
 	public function category()
 	{
 		return $this->belongsTo(Category::class, 'category_id', 'id');
 	}
-	
 	public function region()
 	{
 		return $this->belongsTo(Region::class, 'region_id', 'id');
 	}
-	
 	public function values()
 	{
 		return $this->hasMany(Value::class, 'advert_id', 'id');
 	}
-	
 	public function photos()
 	{
 		return $this->hasMany(Photo::class, 'advert_id', 'id');
 	}
-	
+	public function favorites()
+	{
+		return $this->belongsToMany(User::class, 'advert_favorites', 'advert_id', 'user_id');
+	}
+	public function dialogs()
+	{
+		return $this->hasMany(Dialog::class, 'advert_id', 'id');
+	}
 	public function scopeActive(Builder $query)
 	{
 		return $query->where('status', self::STATUS_ACTIVE);
 	}
-	
 	public function scopeForUser(Builder $query, User $user)
 	{
 		return $query->where('user_id', $user->id);
 	}
-	
 	public function scopeForCategory(Builder $query, Category $category)
 	{
-		return $query->where('user_id', array_merge(
+		return $query->whereIn('category_id', array_merge(
 			[$category->id],
 			$category->descendants()->pluck('id')->toArray()
 		));
 	}
-	
 	public function scopeForRegion(Builder $query, Region $region)
 	{
 		$ids = [$region->id];
@@ -189,6 +229,12 @@ class Advert extends Model
 		while ($childrenIds = Region::where(['parent_id' => $childrenIds])->pluck('id')->toArray()) {
 			$ids = array_merge($ids, $childrenIds);
 		}
-		return $query->where('user_id', $ids);
+		return $query->whereIn('region_id', $ids);
+	}
+	public function scopeFavoredByUser(Builder $query, User $user)
+	{
+		return $query->whereHas('favorites', function(Builder $query) use ($user) {
+			$query->where('user_id', $user->id);
+		});
 	}
 }
--- app/UseCases/Adverts/AdvertService.php
+++ app/UseCases/Adverts/AdvertService.php
@@ -19,14 +19,11 @@ class AdvertService
 	{
 		/** @var User $user */
 		$user = User::findOrFail($userId);
-		
 		/** @var Category $category */
 		$category = Category::findOrFail($categoryId);
-		
 		/** @var Region $region */
 		$region = $regionId ? Region::findOrFail($regionId) : null;
-		
-		return Db::transaction(function () use ($request, $user, $region, $category) {
+		return DB::transaction(function () use ($request, $user, $category, $region) {
 			/** @var Advert $advert */
 			$advert = Advert::make([
 				'title' => $request['title'],
@@ -35,13 +32,10 @@ class AdvertService
 				'address' => $request['address'],
 				'status' => Advert::STATUS_DRAFT,
 			]);
-			
 			$advert->user()->associate($user);
 			$advert->category()->associate($category);
 			$advert->region()->associate($region);
-			
 			$advert->saveOrFail();
-			
 			foreach ($category->allAttributes() as $attribute) {
 				$value = $request['attributes'][$attribute->id] ?? null;
 				if (!empty($value)) {
@@ -51,46 +45,49 @@ class AdvertService
 					]);
 				}
 			}
-			
 			return $advert;
 		});
 	}
-	
-	public function addPhoto($id, PhotosRequest $request): void
+	public function addPhotos($id, PhotosRequest $request): void
 	{
 		$advert = $this->getAdvert($id);
-		
 		DB::transaction(function () use ($request, $advert) {
 			foreach ($request['files'] as $file) {
 				$advert->photos()->create([
-					'file' => $file->store('adverts')
+					'file' => $file->store('adverts', 'public')
 				]);
 			}
+			$advert->update();
 		});
 	}
-	
+	public function edit($id, EditRequest $request): void
+	{
+		$advert = $this->getAdvert($id);
+		$advert->update($request->only([
+			'title',
+			'content',
+			'price',
+			'address',
+		]));
+	}
 	public function sendToModeration($id): void
 	{
 		$advert = $this->getAdvert($id);
 		$advert->sendToModeration();
 	}
-	
 	public function moderate($id): void
 	{
 		$advert = $this->getAdvert($id);
 		$advert->moderate(Carbon::now());
 	}
-	
 	public function reject($id, RejectRequest $request): void
 	{
 		$advert = $this->getAdvert($id);
 		$advert->reject($request['reason']);
 	}
-	
 	public function editAttributes($id, AttributesRequest $request): void
 	{
 		$advert = $this->getAdvert($id);
-		
 		DB::transaction(function () use ($request, $advert) {
 			$advert->values()->delete();
 			foreach ($advert->category->allAttributes() as $attribute) {
@@ -98,20 +95,28 @@ class AdvertService
 				if (!empty($value)) {
 					$advert->values()->create([
 						'attribute_id' => $attribute->id,
-						'value' => $value
+						'value' => $value,
 					]);
 				}
 			}
+			$advert->update();
 		});
 	}
-	
+	public function expire(Advert $advert): void
+	{
+		$advert->expire();
+	}
+	public function close($id): void
+	{
+		$advert = $this->getAdvert($id);
+		$advert->close();
+	}
 	public function remove($id): void
 	{
 		$advert = $this->getAdvert($id);
 		$advert->delete();
 	}
-	
-	public function getAdvert($id): Advert
+	private function getAdvert($id): Advert
 	{
 		return Advert::findOrFail($id);
 	}
