091ff480af74d93d4fd03da6cd9b4069aed90b2a
rupa4ok <rupak4@yandex.ru>
2019-06-23 13:53:25 +0300

    Heroku settings

--- config/database.php
+++ config/database.php
@@ -53,11 +53,11 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-	        'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-	        'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
+            'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
